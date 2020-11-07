706942573f21c9aa5d0cce48a4bca105b30a7f84
EC2 Default User <ec2-user@ip-172-31-3-214.us-east-2.compute.internal>
2020-11-07 10:00:16 +0000

    Laravel11 Bladeテンプレートの継承とLaravel Mixの使い方を活用してみよう

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'Myプロフィール')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
